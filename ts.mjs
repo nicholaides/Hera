f9afe4fa2a1c0ac11f0d12df0ec19405bdd521b9
Mike Nicholaides <mike@nicholaides.com>
2025-04-12 15:18:42 -0400

    Clean up

--- ts.mjs
+++ ts.mjs
@@ -1,13 +1,13 @@
 import { stripTypeScriptTypes } from "node:module";
 
+export const load = transformLoadHook(
+  (url) => /\.hera$/.test(url),
+  stripTypeScriptTypes
+);
+
 export const transformLoadHook = (matchUrl, fn) => async (url, context, next) =>
   next(url, context).then((result) =>
     matchUrl(url)
       ? { ...result, source: fn(result.source, context, result) }
       : result
   );
-
-export const load = transformLoadHook(
-  (url) => /\.hera$/.test(url),
-  stripTypeScriptTypes
-);
